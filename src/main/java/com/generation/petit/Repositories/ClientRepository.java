7bc07271379c6253d1e2203de81a707a97c58538
MarlenLuna <masluss2903@gmail.com>
2020-02-26 08:57:55 +0700

    Added all repositories

---
+++ src/main/java/com/generation/petit/Repositories/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.generation.petit.Repositories;
+
+import com.generation.petit.Models.Client;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClientRepository extends CrudRepository<Client, Integer>{
+    
+}
---
+++ src/main/java/com/generation/petit/Repositories/VeterinaryRepository.java
@@ -0,0 +1,9 @@
+package com.generation.petit.Repositories;
+
+import com.generation.petit.Models.Veterinary;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface VeterinaryRepository extends CrudRepository<Veterinary, Integer> {
+    
+}
